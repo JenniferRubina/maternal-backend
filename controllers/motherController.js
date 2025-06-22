e7582b650eb7672efed76a13f17b72d6d94ff5b8
JenniferRubina <161840480+JenniferRubina@users.noreply.github.com>
2025-06-22 11:02:42 +0530

    Update motherController.js

--- controllers/motherController.js
+++ controllers/motherController.js
@@ -2,7 +2,7 @@
 const pool = require('../db');
 exports.getAllMothers = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM mother');
+    const result = await pool.query('SELECT * FROM mother_details');
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
