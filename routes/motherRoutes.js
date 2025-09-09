1e038a56ca56c730aec631ad4c43f8603256d0ea
linkesh2004 <147417243+linkesh2004@users.noreply.github.com>
2025-09-09 14:09:45 +0530

    Update motherRoutes.js

--- routes/motherRoutes.js
+++ routes/motherRoutes.js
@@ -155,7 +155,7 @@ router.get('/mother-details', async (req, res) => {
 
   try {
     const result = await pool.query(
-      `SELECT hb, bp_systolic, bp_diastolic FROM public.anc_visit WHERE rch_id = $1`, [rch_id]
+      `SELECT hb, weight, bp_systolic, bp_diastolic FROM public.anc_visit WHERE rch_id = $1`, [rch_id]
     );
     if (result.rows.length > 0) {
       res.status(200).json(result.rows);
